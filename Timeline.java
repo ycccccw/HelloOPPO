4121be9d6f9bc57489f1ba9157d1c9592ac6276e
ycccccw <ycccccw@outlook.com>
2023-03-06 07:33:37 +0800

    1

---
+++ Timeline.java
@@ -0,0 +1,18 @@
+public class Timeline {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+		
+		System.out.println("You were born in 1995");
+		
+		//Sun MIcrosystems announced the release of Java in 1995
+		
+		System.out.println("You were credted by James GosLing");
+		/*
+		James Gosling is a Canadian engineer who 
+		created Java while working at Sun Microsystems. 
+		His favorite number is the square root of 2!
+		 */
+		System.out.println("You are a fun Language!");
+		
+	}
+}
