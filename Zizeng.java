5de9aefa82a7520736809f4deb681978d13755d8
ycccccw <ycccccw@outlook.com>
2023-03-06 07:31:57 +0800

    1

---
+++ Zizeng.java
@@ -0,0 +1,10 @@
+public class Zizeng {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = ++a;
+		int c = 3;
+		int d = --c;
+		System.out.println("自增运算后的值是" + b);
+		System.out.println("自减运算后的值是" + d);
+	}
+}
