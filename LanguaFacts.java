bfa14036b6435b0ececa58aa827e179991f92bed
ycccccw <ycccccw@outlook.com>
2023-03-06 18:20:04 +0800

    11

---
+++ LanguaFacts.java
@@ -0,0 +1,14 @@
+public class LanguaFacts {
+	public static void main(String[] args) {
+		// press enter or return on your keyboard after each semicolon!
+		
+		System.out.println("java is a class-vased language.");
+		System.out.println("java classes have a'main' method");
+		System.out.println("java statements end with a semicolon.");
+		
+		System.out.println("Programming is… ");
+		
+				
+				
+	}
+}
