9f92dd17043dad242e23f03c6bc26b48b27b3c82
ycccccw <ycccccw@outlook.com>
2023-03-08 09:56:58 +0800

    11

---
+++ Creator.java
@@ -0,0 +1,11 @@
+public class Creator {
+	public static void main(String[] args) {
+		String name = "James GosLing";
+		int yearCreated = 1995;
+		
+		System.out.println(name);
+		System.out.println(yearCreated);
+		
+		
+	}
+}
