974d0e1bce4bcc5d0676ec9ae8baff8bd51aa1d1
ycccccw <ycccccw@outlook.com>
2023-03-09 10:01:25 +0800

    11

---
+++ MyProfile.java
@@ -0,0 +1,10 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "chengWei";
+    int age = 21;
+    double desiredSalary = 250000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+
+	}
+}
