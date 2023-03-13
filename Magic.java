98b39c991b97ecd472c59b48be738bd68aedab74
ycccccw <ycccccw@outlook.com>
2023-03-13 10:15:24 +0800

    11

---
+++ Magic.java
@@ -0,0 +1,16 @@
+public class Magic {
+	public static void main(String[] args) {
+    int myNumber = 2;
+    //这是原始数
+    int stepOne = myNumber * 2;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix =  stepFive / 6;
+    System.out.println(stepSix);
+
+		
+
+	}
+}
---
+++ myMagic.java
@@ -0,0 +1,8 @@
+public class myMagic {
+	public static void main(String[] args) {
+		int myNumber = 2;
+		int myMagic =(((((myNumber + myNumber +myNumber) / 2) +17) -2) / 6);
+		System.out.println(myMagic);
+		
+	}
+}
